8d47f0604bc063ab0e43acde6f7afd701f61d96d
zek <zek@netcompany.com>
2026-04-10 22:21:59 +0200

    Simple catmovement added

--- ngj-2026-nothing-do-cat/CatController.gd
+++ ngj-2026-nothing-do-cat/CatController.gd
@@ -1,20 +1,33 @@
 extends CharacterBody2D
 
 
+var speed = 4;
+var jumpSpeed = 8;
 
+func _ready() -> void:
+	pass
 
 func _physics_process(delta: float) -> void:
-	velocity = Vector2.UP * Globals.catTileSize.y * 5;
+	velocity += Vector2.DOWN * 10;
+	get_input()
 	move_and_slide()
 
-"""
+
 func _input(event):
 	if event.is_action_pressed("left"):
-		rotation_degrees = 0;
+		#velocity.x = Vector2.LEFT.x * 32 * 2;
+		pass
 	else: if event.is_action_pressed("right"):
-		rotation_degrees = 180;
-	else: if event.is_action_pressed("up"):
-		rotation_degrees = 90;
-	else: if event.is_action_pressed("down"):
-		rotation_degrees = 270;
-"""
+		#velocity.x = Vector2.RIGHT.x * 32 * 2;
+		pass
+	else:
+		velocity.x = 0;
+	if event.is_action_pressed("jump"):
+		velocity.y = Vector2.UP.y * 32 * jumpSpeed;
+		pass
+		
+
+
+func get_input():
+	var move = transform.x * Input.get_axis("left", "right") * 32 * speed
+	velocity.x = move.x
