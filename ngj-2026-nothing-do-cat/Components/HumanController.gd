583dca94348968fc6f1b54bedc5d66fda710ac7a
zek <zek@netcompany.com>
2026-04-11 13:25:16 +0200

    Human Movement with sprite flip

--- ngj-2026-nothing-do-cat/Components/HumanController.gd
+++ ngj-2026-nothing-do-cat/Components/HumanController.gd
@@ -1,17 +1,23 @@
 extends Node2D
 
 @onready var characterBody = $CharacterBody2D
+@onready var animated_sprite = $CharacterBody2D/AnimatedSprite2D
 
 enum states
 {
-	IDLE,
-	MOVING,
+	IDLE = 0,
+	MOVING = 1,
+	WORKING = 2,
 }
 var state : states
 
 @export var speed = 1.5
 @export var offset = 2;
 @export var idleTime = 5;
+
+@export var startPosition = 200
+@export var standingHeight = 144
+@export var walkingHeight = 146;
 
 var targetPosition: int;
 var placeholderTargets = []
@@ -23,6 +29,11 @@ func _ready() -> void:
 	placeholderTargets.push_front(400)
 	placeholderTargets.push_front(100)
 	state = states.IDLE;
+	print(str(states.IDLE))
+	animated_sprite.animation = "IDLE" 
+	characterBody.global_position.x = startPosition;
+	characterBody.global_position.y = standingHeight;
+	Globals.humanPosition = characterBody.global_position
 
 func _process(delta: float) -> void:
 	stateTime += delta;
@@ -32,7 +43,8 @@ func _process(delta: float) -> void:
 			if(stateTime > idleTime):
 				stateTime = 0;
 				state = states.MOVING;
-				print("MOVING")
+				animated_sprite.animation = "MOVING";
+				characterBody.global_position.y = walkingHeight;
 				
 				#PLACEHOLDER LOGIC
 				if(targetPosition != null && targetPosition != 0):
@@ -46,17 +58,20 @@ func _process(delta: float) -> void:
 			if(abs(distanceFromTarget) < offset):
 				stateTime = 0;
 				state = states.IDLE;
+				animated_sprite.animation = "IDLE"
+				characterBody.global_position.y = standingHeight;
 				print("IDLE")
 				characterBody.velocity.x = 0;
 			else:
 				if(targetPosition > Globals.humanPosition.x):
 					#Moving Right
-					#animated_sprite.flip_h = false
+					animated_sprite.flip_h = true
 					characterBody.velocity.x = Globals.tileSize.x * speed;
 					pass
 				else:
 					#Moving Left
 					characterBody.velocity.x = -Globals.tileSize.x * speed;
+					animated_sprite.flip_h = false
 				pass
 			pass
 		pass
