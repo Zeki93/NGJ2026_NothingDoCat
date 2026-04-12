49d81abafe6361137ef1114e11d8ace18af6158b
zek <zek@netcompany.com>
2026-04-12 12:11:44 +0200

    Added endscreen menu

--- ngj-2026-nothing-do-cat/Components/Gamestate/GamestateController.gd
+++ ngj-2026-nothing-do-cat/Components/Gamestate/GamestateController.gd
@@ -4,10 +4,23 @@ extends Node2D
 var bedroomDoorOpened = false;
 @onready var door_sprite = $Door/StaticBody2D/Sprite2D;
 @onready var door_collision = $Door/StaticBody2D/CollisionShape2D
+@onready var endscreen_menu = $"../EndMenu";
 
+var endscreen_menu_timer = 0;
+var show_menu_after_seconds = 5;
 
 
 func _process(delta: float) -> void:
+	if(Globals.gameEnd):
+		endscreen_menu_timer += delta;
+		if(endscreen_menu_timer > show_menu_after_seconds):
+			endscreen_menu.visible = true;
+	else:
+		check_for_game_end();
+	
+
+func check_for_game_end():
+	endscreen_menu.visible = false;
 	if(Globals.human_interrupted_counter >= interrupts_to_end_game):
 		if(!bedroomDoorOpened):
 			_open_bedroom_door()
